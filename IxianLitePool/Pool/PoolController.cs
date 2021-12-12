7c1aa9cc126a6e8619376184d156049e5889cf1b
bogdanadnan <bogdan_adnan@yahoo.com>
2021-12-12 17:50:55 +0200

    - missing files

---
+++ IxianLitePool/Pool/PoolController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Collections;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace LP.Pool
+{
+    public class Pool
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Salary { get; set; }
+    }
+
+    public class PoolController : ApiController
+    {
+        Pool[] employees = new Pool[]
+        {
+            new Pool{Id=0,Name="Morris",Salary="151110"},
+            new Pool{Id=1,Name="John", Salary="120000"},
+            new Pool{Id=2,Name="Chris",Salary="140000"},
+            new Pool{Id=3,Name="Siraj", Salary="90000"}
+        };
+
+        public IEnumerable<Pool> Get()
+        {
+            return employees.ToList();
+        }
+
+        public Pool Get(int Id)
+        {
+            try
+            {
+                return employees[Id];
+            }
+            catch (Exception)
+            {
+                return new Pool();
+            }
+        }
+
+    }
+}
---
+++ IxianLitePool/Pool/Startup.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Owin;
+using System.Web.Http;
+using System.Net.Http;
+using Microsoft.Owin;
+using Microsoft.Owin.StaticFiles;
+using Microsoft.Owin.FileSystems;
+
+[assembly: OwinStartup(typeof(LP.Pool.Startup))]
+namespace LP.Pool
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder appBuilder)
+        {
+            HttpConfiguration config = new HttpConfiguration();
+            config.Routes.MapHttpRoute(
+                name: "createUserApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+                );
+            appBuilder.UseWebApi(config);
+
+            var options = new FileServerOptions
+            {
+                EnableDirectoryBrowsing = true,
+                EnableDefaultFiles = true,
+                DefaultFilesOptions = { DefaultFileNames = { "index.html" } },
+                FileSystem = new PhysicalFileSystem("wwwroot")
+            };
+
+            appBuilder.UseFileServer(options);
+        }
+    }
+}
